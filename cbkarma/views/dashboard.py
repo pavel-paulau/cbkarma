c6e38a4f9aac85dbebee550bb39d6a79600559b5
Pavel.Paulau <pavel.paulau@gmail.com>
2012-06-04 12:13:53 -0700

    read data from database for summary table

--- cbkarma/views/dashboard.py
+++ cbkarma/views/dashboard.py
@@ -1,5 +1,27 @@
 from pyramid.view import view_config
+from cbkarma.data.client import CbClient
 
 @view_config(route_name='home', renderer='/home/core.mako')
 def home(request):
-    return {}
+    # Couchbase client
+    client = CbClient()
+
+    # Output dictionary
+    data = list()
+
+    for row in client.query(ddoc='karma', view='uid')['rows']:
+        test = client.find(str(row['id']))
+
+        latest_timestamp = max(test['events'].keys())
+        status = "{0}: {1}".format(test['events'][latest_timestamp]['phase'],
+                                   test['events'][latest_timestamp]['status'])
+
+        data.append({'test_id': row['id'],
+                     'build': test['build'],
+                     'spec': test['spec'],
+                     'description': test['description'],
+                     'status': status,
+                     'timestamp': latest_timestamp
+        })
+
+    return {'data': data}

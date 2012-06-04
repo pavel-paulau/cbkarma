67b1e4b679af34a71b240b4731b344a8353cfee2
Pavel.Paulau <pavel.paulau@gmail.com>
2012-06-04 12:08:38 -0700

    added rest client to couchbase client

--- cbkarma/data/client.py
+++ cbkarma/data/client.py
@@ -1,17 +1,28 @@
 import json
 
 from cbkarma.lib.couchbase.couchbaseclient import VBucketAwareCouchbaseClient
+from cbkarma.lib.couchbase.rest_client import RestConnection
 
 class CbClient:
     """Abstract couchbase client"""
 
     def __init__(self, host='localhost', port='8091', bucket='default',
-                 password='password'):
+                 username='Administrator', password='password'):
         protocol = "http://"
         api = "/pools/default"
         uri = protocol + host + ":" + port + api
 
         self.client = VBucketAwareCouchbaseClient(uri, bucket, password, True)
+
+        self.bucket = bucket
+
+        server = {
+            'ip': host,
+            'port': port,
+            'username': username,
+            'password': password,
+        }
+        self.rest_client = RestConnection(server)
 
     def insert(self, id, doc={}):
         doc = json.dumps(doc)
